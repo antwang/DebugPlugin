3b6cc2dff5720973017672de96571bc78182a999
wangchao <wangchao@daojia.com>
2019-06-05 17:09:05 +0800

    rewrite code

--- DebugPlugin.js
+++ DebugPlugin.js
@@ -1,54 +1,53 @@
 const fs = require("fs");
 const path = require("path");
+const SingleEntryPlugin = require("webpack/lib/SingleEntryPlugin");
+const MultiEntryPlugin = require("webpack/lib/MultiEntryPlugin");
 
+// webpack内置的处理入口文件的插件
+const itemToPlugin = (context, item, name) => {
+  if (Array.isArray(item)) {
+    return new MultiEntryPlugin(context, item, name);
+  }
+  return new SingleEntryPlugin(context, item, name);
+};
 class DebugPlugin {
   constructor(options) {
     this.options = Object.assign({}, { enable: false }, options);
   }
   apply(compiler) {
-    const ID = `vc-debug`;
-    compiler.hooks.entryOption.tap(ID, this.addVConsole.bind(this));
-  }
-
-  addVConsole(localPath, entry) {
-    let vconslePath = "debugtool-webpack-plugin/vconsole.js";
-    const { enable } = this.options;
-    const _exist = module.parent.paths.find(item => {
-      let _vconslePath = path.join(item, vconslePath);
-      if (fs.existsSync(item) && fs.existsSync(_vconslePath)) {
-        vconslePath = _vconslePath;
-        return true;
-      }
-      return false;
-    });
-
-    if (!_exist) {
-      console.error("debugtool-webpack-plugin/vconsole.js not found!");
-      return;
-    }
-
-    if (enable) {
-      if (typeof entry === "string") {
-        entry = [vconslePath, entry];
-        console.error("entry could not be a string");
-        return;
-      }
-
-      if (Object.prototype.toString.call(entry) === "[object Array]") {
-        entry.unshift(vconslePath);
-      }
-
-      if (Object.prototype.toString.call(entry) === "[object Object]") {
-        for (let key in entry) {
-          if (Object.prototype.toString.call(entry[key]) === "[object Array]") {
-            entry[key].unshift(vconslePath);
+    compiler.hooks.entryOption.tap("vc-debug", (context, entry) => {
+      const { enable } = this.options;
+      let vconslePath = "debugtool-webpack-plugin/vconsole.js";
+      if (enable) {
+        module.parent.paths.find(item => {
+          let _vconslePath = path.join(item, vconslePath);
+          if (fs.existsSync(item) && fs.existsSync(_vconslePath)) {
+            vconslePath = _vconslePath;
+            return true;
           }
-          if (typeof entry[key] === "string") {
-            entry[key] = [vconslePath, entry[key]];
+          return false;
+        });
+
+        // 将vconsole添加到entry中
+        if (typeof entry === "string") {
+          entry = [vconslePath, entry];
+        } else if (Array.isArray(entry)) {
+          entry.unshift(vconslePath);
+        } else if (typeof entry === "object") {
+          entry[key] = vconslePath;
+        }
+
+        // 使用entryPlugin重新解析entry
+        if (typeof entry === "string" || Array.isArray(entry)) {
+          itemToPlugin(context, entry, "app").apply(compiler);
+        } else if (typeof entry === "object") {
+          for (const name of Object.keys(entry)) {
+            itemToPlugin(context, entry[name], name).apply(compiler);
           }
         }
       }
-    }
+      return true;
+    });
   }
 }
 
